e3e8c81120a6004a57a435d8e679712b79d67354
spaceTony9 <bilokon.fw7@gmail.com>
2024-02-19 00:17:22 +0100

    upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,24 @@
 import axios from 'axios';
 import { CONSTANTS } from './constants.js';
 
-export async function urlCreator() {
-  try {
-    const getUrl = await axios.get(CONSTANTS.API_URL, {
+export async function urlCreator(searchedPhoto) {
+  const getUrl = await axios.get(CONSTANTS.API_URL, {
       params: {
         key: CONSTANTS.API_KEY,
-        q: null,
+        q: searchedPhoto,
         image_type: CONSTANTS.IMAGE_TYPE,
         orientation: CONSTANTS.PHOTO_ORIENTATION,
         safesearch: CONSTANTS.SAFESEARCH,
       },
-    });
-  } catch (error) {
-    showErrorPopUp(CONSTANTS.ERROR_MESSAGES.RESOURSE_ERROR);
-  }
+    })
+  return getUrl;
 }
-export const getUrl = axios.get(CONSTANTS.API_URL, {
-  params: {
-    key: CONSTANTS.API_KEY,
-    q: null,
-    image_type: CONSTANTS.IMAGE_TYPE,
-    orientation: CONSTANTS.PHOTO_ORIENTATION,
-    safesearch: CONSTANTS.SAFESEARCH,
-  },
-});
+// export const getUrl = axios.get(CONSTANTS.API_URL, {
+//   params: {
+//     key: CONSTANTS.API_KEY,
+//     q: null,
+//     image_type: CONSTANTS.IMAGE_TYPE,
+//     orientation: CONSTANTS.PHOTO_ORIENTATION,
+//     safesearch: CONSTANTS.SAFESEARCH,
+//   },
+// });
