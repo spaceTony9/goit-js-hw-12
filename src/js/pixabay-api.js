65d4afd6c9a52a7022c914ad989c283b1eb2510c
spaceTony9 <bilokon.fw7@gmail.com>
2024-02-19 22:14:59 +0100

    upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import { CONSTANTS } from './constants.js';
 
-export async function urlCreator(searchedPhoto) {
+export async function urlCreator(searchedPhoto, pageParam) {
   const getUrl = await axios.get(CONSTANTS.API_URL, {
-      params: {
-        key: CONSTANTS.API_KEY,
-        q: searchedPhoto,
-        image_type: CONSTANTS.IMAGE_TYPE,
-        orientation: CONSTANTS.PHOTO_ORIENTATION,
-        safesearch: CONSTANTS.SAFESEARCH,
-      },
-    })
+    params: {
+      key: CONSTANTS.API_KEY,
+      q: searchedPhoto,
+      image_type: CONSTANTS.IMAGE_TYPE,
+      orientation: CONSTANTS.PHOTO_ORIENTATION,
+      safesearch: CONSTANTS.SAFESEARCH,
+      page: pageParam,
+      per_page: CONSTANTS.PHOTOS_PER_PAGE,
+    },
+  });
   return getUrl;
 }
 // export const getUrl = axios.get(CONSTANTS.API_URL, {
--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ const input = document.querySelector('.input');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
+let pageParam = 1;
+let inputValue = [];
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -22,20 +24,38 @@ form.addEventListener('submit', e => {
   } else {
     loader.classList.remove('hidden');
 
-    urlCreator(input.value)
-      .then(({data : {hits}}) => {
-      if (!hits.length) {
-        showErrorPopUp(CONSTANTS.ERROR_MESSAGES.IMAGES_NOT_FOUND);
-      } else {
-        gallery.insertAdjacentHTML('beforeend', htmlMarkupCreator(hits));
-        galleryLightBox.refresh();
-      }
-      loader.classList.add('hidden');
-    }).catch(error => {
-      console.debug(error);
-      showErrorPopUp(CONSTANTS.ERROR_MESSAGES.RESOURSE_ERROR);
-    })
+    urlCreator(input.value, pageParam)
+      .then(({ data: { hits } }) => {
+        if (!hits.length) {
+          showErrorPopUp(CONSTANTS.ERROR_MESSAGES.IMAGES_NOT_FOUND);
+        } else {
+          inputValue.push(input.value);
+          console.log(inputValue);
+          gallery.insertAdjacentHTML('beforeend', htmlMarkupCreator(hits));
+          galleryLightBox.refresh();
+        }
+        loader.classList.add('hidden');
+      })
+      .catch(error => {
+        console.debug(error);
+        showErrorPopUp(CONSTANTS.ERROR_MESSAGES.RESOURSE_ERROR);
+      });
   }
+  if (inputValue.length >= 2) {
+    console.log(
+      inputValue[inputValue.length - 1],
+      inputValue[inputValue.length - 2]
+    );
+    if (
+      inputValue[inputValue.length - 1] !== inputValue[inputValue.length-2]
+    ) {
+      inputValue = [];
+      console.log(inputValue);
+      pageParam = 0;
+    }
+  }
+  pageParam++;
+  console.log(pageParam);
 });
 
 function showErrorPopUp(message) {
